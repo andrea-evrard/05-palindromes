325074a92e5ee4c122e4f8caf0477d3e5723019e
andrea-evrard <andrea.evrard@edu.esiee.fr>
2025-09-22 15:53:04 +0000

    travail v1

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 #### Fonction secondaire
-
-
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
+    p=p.lower()
+    translator=str.maketrans("éêèëçàô-!?:',","eeeecao      ")
+    p=p.translate(translator)
+    p=p.replace(" ","")
+    return p==p[::-1]
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
